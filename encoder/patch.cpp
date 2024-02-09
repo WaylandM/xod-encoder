5561780941f3a6c75713ef2c642a61be82d7de0d
WaylandM <mw283@cam.ac.uk>
2024-02-09 17:16:16 +0000

    new file:   encoder-optimized/patch.cpp 	new file:   encoder-optimized/patch.xodp 	new file:   encoder/patch.cpp 	new file:   encoder/patch.xodp 	new file:   example-encoder-optimized/patch.xodp 	new file:   example-encoder/patch.xodp 	new file:   project.xod

---
+++ encoder/patch.cpp
@@ -0,0 +1,25 @@
+// Tell XOD where it could download the library:
+#pragma XOD require "https://github.com/PaulStoffregen/Encoder"
+
+//Include C++ libraries
+#include <Encoder.h>
+
+node {
+    meta {
+        // Define our custom type as a pointer on the class instance.
+        using Type = Encoder*;
+    }
+    
+    static_assert(isValidDigitalPort(constant_input_P1), "must be a valid digital port");
+    static_assert(isValidDigitalPort(constant_input_P2), "must be a valid digitial port");
+
+    Encoder enc = Encoder(constant_input_P1, constant_input_P2);
+
+    void evaluate(Context ctx) {
+
+        if (isInputDirty<input_Read>(ctx)) {
+            emitValue<output_Pos>(ctx, enc.read());
+            emitValue<output_Done>(ctx, 1);
+        }
+    }
+}
